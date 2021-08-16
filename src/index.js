060b9c010f46b2b40f16bea620f8fa936ac75794
Arthur <arthurarp@hotmail.com>
2021-08-15 22:36:25 -0300

    [12] Add mongodb connection
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const {
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+} = process.env;
+
+const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.log('Error on connecting to MongoDB', err);
+  });
 
 const app = express();
 
@@ -6,4 +26,4 @@ app.get('/', (req, res) => {
   res.send('API ROLES!!!');
 });
 
-app.listen(3005);
+app.listen(DB_PORT);
