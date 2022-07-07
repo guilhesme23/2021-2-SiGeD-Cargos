8e278e537f56fd74e85e9bb217de6f312632bfdb
Henrique Melo <hacmelo@gmail.com>
2022-07-06 21:37:07 -0300

    refactor: creating a separate file for the mongo connection

--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const db = require("./config/dbConnect.js");
 require('dotenv').config();
 
-const {
-  DB_USER,
-  DB_PASS,
-  DB_NAME,
-  DB_HOST,
-  DB_PORT,
-} = process.env;
+const { PORT } = process.env;
 
-const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`;
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB is connected');
-  })
-  .catch((err) => {
-    console.log('Error on connecting to MongoDB', err);
-  });
+db.on("error", console.log.bind(console, 'Error on connecting to MongoDB'));
+db.once("open", () => console.log('MongoDB is connected'));
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(DB_PORT, () => {
-  console.log(`Server running on port ${DB_PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
